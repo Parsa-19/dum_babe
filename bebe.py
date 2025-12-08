6bdaf4b336933724c1efc8d886fc883f49a43a9b
Parsa-19 <parsakhalife19@gmail.com>
2025-12-08 15:27:37 +0330

    add bouncing ball

--- bebe.py
+++ bebe.py
@@ -37,6 +37,20 @@ def password_generator(pass_len):
     print(rand_pass)
     return rand_pass
 
+def bouncing_ball():
+    a,b,c,d,e,n='+- |*\n'
+    w=d+c*18+d+n
+    s=a+b*18+a+n
+    x,y=0,0
+    g,h=17,7
+    j,k=1,1
+    count_sec = 0
+    while count_sec < 10:
+        if 0>x or x>g:j*=-1;x+=j
+        if 0>y or y>h:k*=-1;y+=k
+        os.system('cls');print( s+w*y+d+c*x+e+c*(g-x)+d+n+w*(h-y)+s );x+=j;y+=k;time.sleep(0.1)
+        count_sec += 0.1
+
 if __name__ == "__main__":
     choise = input("""
 Choose function to run 
